82f93db810beedb4f0cced519c31a1319bf9a517
Sushant <stha2022sushant@gmail.com>
2025-01-16 16:39:38 +0545

    pass test case for createBlog

--- blogApplication/test_blog.py
+++ blogApplication/test_blog.py
@@ -2,47 +2,12 @@ import json
 from django.test import TestCase, Client
 from . factories import BlogFactory
 from .models import BlogApp
-
-"""
 class TestBlogMutation(TestCase):
     def setUp(self):
         self.client = Client()
         self.create_blog_mutation = '''
         mutation MyMutation {
-                createBlog(data: {title: "test", blog: "test"}) {
-                id
-                blog
-                title
-            }
-        }
-        '''
-       
-
-    def test_create_blog(self):
-        response = self.client.post(
-            '/graphql',
-            json.dumps(
-                {
-                    'query': self.create_blog_mutation
-                    
-                }
-            ),
-            content_type='application/json'
-        )
-        data = json.loads(response.content)
-        print("################################")
-        print(data)
-        print("################################")
-        self.assertTrue(True)
-
-        """
-
-class TestBlogMutation(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.create_blog_mutation = '''
-        mutation MyMutation {
-            createBlog(data: {title: "a", blog: "test"}) {
+            createBlog(data: {title: "test", blog: "test"}) {
                 id
                 blog
                 title
@@ -52,26 +17,22 @@ class TestBlogMutation(TestCase):
             }
         }
         '''
-        self.blog_with_author = BlogFactory()  # Blog with an author (user created)
-        self.blog_without_author = BlogFactory(author=None)  # Blog without an author (author is None)
+        self.blog_with_author = BlogFactory()
 
     def test_create_blog_with_author(self):
         response = self.client.post(
             '/graphql',
             json.dumps(
                 {
-                    'query': self.create_blog_mutation.replace(
-                        "test", self.blog_with_author.title
-                    ).replace("test", self.blog_with_author.blog)
+                    'query': self.create_blog_mutation
                 }
             ),
             content_type='application/json'
         )
-        data = json.loads(response.content)
-        print("################################")
-        print(data)
-        print("################################")
-        self.assertTrue(True)
+        data = json.loads(response.content)['data']
+        self.assertEqual(data['createBlog']['blog'], 'test')
+        self.assertEqual(data['createBlog']['title'], 'test')
+        
 
     
 

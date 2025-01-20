73a71145c5204229274b3289e1d12b5fb250d362
sushant <stha2022sushant@gmail.com>
2025-01-20 16:08:12 +0545

    complete CRUD

--- blogApplication/schema.py
+++ blogApplication/schema.py
@@ -1,6 +1,8 @@
 import strawberry
 import strawberry_django
 from strawberry_django import auth, mutations
+from asgiref.sync import sync_to_async
+from . models import BlogApp as BlogAppModel
 
 from .types import (
     BlogApp,
@@ -19,8 +21,34 @@ class Query:
 class Mutation:
     create_blog: BlogApp = mutations.create(BlogAppInput)
     create_blogs: list[BlogApp] = mutations.create(BlogAppInput)
-    update_blogs: list[BlogApp] = mutations.update(BlogAppPartialInput)
-    delete_blogs: list[BlogApp] = mutations.delete()
+
+    #update_blogs: list[BlogApp] = mutations.update(BlogAppPartialInput)
+
+    @strawberry.mutation
+    @sync_to_async
+    def update_blog(self, info, blog_data: BlogAppPartialInput) -> BlogApp:
+        blog_instance = BlogAppModel.objects.get(id=blog_data.id)
+        
+        if blog_data.title:
+            blog_instance.title = blog_data.title
+        if blog_data.blog:
+            blog_instance.blog = blog_data.blog
+        #if blog_data.author:
+        #    blog_instance.author = blog_data.author
+        
+        blog_instance.save()
+        return blog_instance
+    #delete_blogs: list[BlogApp] = mutations.delete()
+
+    @strawberry.mutation
+    @sync_to_async
+    def delete_blog(self, info, blog_id: int) -> str:
+        try:
+            blog_instance = BlogAppModel.objects.get(id=blog_id)
+            blog_instance.delete()
+            return f"Blog with ID {blog_id} has been deleted successfully."
+        except BlogAppModel.DoesNotExist:
+            return f"Blog with ID {blog_id} does not exist."
 
 
     

6641068d67eb511fc89d6e2b473fa4fd9946bd13
sushant <stha2022sushant@gmail.com>
2025-01-23 16:23:06 +0545

    integrated : BlogFactory() into update test case

--- tests/test_blog_restapi.py
+++ tests/test_blog_restapi.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 from blogApplication.models import BlogApp
+from blogApplication.factories import BlogFactory
 
 
 class BlogIntegrationTestCase(TestCase):
@@ -13,11 +14,14 @@ class BlogIntegrationTestCase(TestCase):
         )
 
     def test_create_blog(self):
+        
         """Integration test for creating a blog post"""
         payload = {
             "title": "Integration Test Blog",
             "blog": "This is an integration test blog content."
         }
+        
+        #payload = BlogFactory()
         response = self.client.post("/add/", payload, format="json")
         self.assertEqual(response.status_code, 201)
         self.assertEqual(BlogApp.objects.count(), 2)
@@ -26,18 +30,20 @@ class BlogIntegrationTestCase(TestCase):
         self.assertEqual(created_blog.blog, payload["blog"])
 
     def test_update_blog(self):
-        """Integration test for updating a blog post"""
+        """Integration test for updating a blog post using BlogFactory"""
+        # Generate new dynamic data for update
+        new_data = BlogFactory.build()  # Builds a BlogFactory instance without saving it
         payload = {
-            "title": "Updated Blog Title",
-            "blog": "This is updated blog content."
+            "title": new_data.title,
+            "blog": new_data.blog,
         }
         response = self.client.post(f"/update/{self.blog.id}/", payload, format="json")
         self.assertEqual(response.status_code, 200)
 
         # Verify the blog is updated in the database
         updated_blog = BlogApp.objects.get(id=self.blog.id)
-        self.assertEqual(updated_blog.title, payload["title"])
-        self.assertEqual(updated_blog.blog, payload["blog"])
+        self.assertEqual(updated_blog.title, new_data.title)
+        self.assertEqual(updated_blog.blog, new_data.blog)
 
     def test_delete_blog(self):
         """Integration test for deleting a blog post"""
